936063a18541778b0b9050404febf9588687d83b
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2025-01-06 18:34:45 +0100

    fix/dropdown: Fix duplicate key for dropdown when using same value for options

--- src/components/ui/Input/Dropdown.tsx
+++ src/components/ui/Input/Dropdown.tsx
@@ -2,8 +2,8 @@ import { HtmlHTMLAttributes } from "react";
 
 export const Dropdown = ({ options, value, onChange, ...props }: { options: string[], value: string, onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void } & HtmlHTMLAttributes<HTMLSelectElement>) => {
   return <select value={value} onChange={onChange} {...props}>
-    {options.map((option) => {
-      return <option key={option} value={option}>{option}</option>
+    {options.map((option, index) => {
+      return <option key={index} value={option}>{option}</option>
     })}
   </select>
 }
